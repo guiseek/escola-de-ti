d418e6404699596abaf25f8c7e9b6e6b37990245
Gui Seek <guiseek@gmail.com>
2023-07-26 15:29:16 -0300

    feat: add user dtos

--- apps/back/auth/src/app/users/users.service.ts
+++ apps/back/auth/src/app/users/users.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { User } from './schema/user.schema'
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { User } from './schema/user.schema';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = this.userModel.create(createUserDto)
+    const createdUser = this.userModel.create(createUserDto);
 
-    return createdUser
+    return createdUser;
   }
 
   async findOne(name: string) {
-    const findedUser = this.userModel.findOne({ name: name })
+    const findedUser = this.userModel.findOne({ name: name });
 
-    return findedUser
+    return findedUser;
   }
-
-
 }
