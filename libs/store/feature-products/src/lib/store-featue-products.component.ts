1e93b457a4c8c1bda86ddc3640b48a9796a9017d
gabriel gomes <gabrielluizgomes1@gmail.com>
2023-07-31 22:09:36 -0300

    feat: 🎸 adcionar salvar products no banco

--- libs/store/feature-products/src/lib/store-featue-products.component.ts
+++ libs/store/feature-products/src/lib/store-featue-products.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
@@ -7,6 +8,8 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./store-featue-products.component.scss'],
 })
 export class StoreFeatueProductsComponent {
+  constructor(private http: HttpClient) {}
+
   form = new FormGroup({
     name: new FormControl(''),
     description: new FormControl(''),
@@ -14,9 +17,17 @@ export class StoreFeatueProductsComponent {
     quantity: new FormControl(''),
   });
 
+  products$ = this.http.get('/api/products');
+
   onSubmit() {
-    const newElement = document.createElement('pre');
-    newElement.innerHTML = JSON.stringify(this.form.value, null, 2);
-    document.body.appendChild(newElement);  
+    if (this.form.valid) {
+      const data = {
+        name: this.form.value.name,
+        description: this.form.value.description,
+        price: Number(this.form.value.price),
+        quantity: Number(this.form.value.quantity),
+      }
+      this.http.post('/api/products', data).subscribe(console.log);
+    }
   }
 }
